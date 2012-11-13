2bb89d09731f3df9e65860548fd8827e3a16a1fc
Sarven Capadisli <info@csarven.ca>
2012-11-13 18:15:13 +0100

    Added plot title

--- R.cor.complete.pearson.r
+++ R.cor.complete.pearson.r
@@ -2,12 +2,26 @@
 #Author: Sarven Capadisli <info@csarven.ca>
 #Author URL: http://csarven.ca/#i
 
+args <- commandArgs(TRUE)
+argv <- length(args)
+#if (is.null(argv) | length(argv)<1) {
+#  cat("Usage: installr.r pkg1 [pkg2 pkg3 ...]0")
+#  q()
+#}
+#print(args)
+
 library(ggplot2)
+indicators <- read.csv("indicators-notations-prefLabels.csv", header=T)
+
 files <- dir("data/", "*.csv")
 for (f in files) {
     data <- read.csv(paste("data/", f, sep=""), header=T)
     results <- cor(data, use="complete.obs", method="pearson")
     cat(paste(f, results[2], sep=","), file="coefficients.csv", sep="\n", append=TRUE)
-    p <- ggplot(data, aes(indicatorValue, CPIRank))
-    ggsave(p + geom_point() + stat_smooth(), file=paste("charts/", f, ".jpg", sep=""), scale=0.75)
+
+    n <- substr(f, 0, nchar(f, type="chars")-9)
+
+    title <- paste(indicators[indicators$notation == n,2], "2010", sep=", ")
+
+    ggsave(ggplot(data, aes(indicatorValue, CPIRank)) + geom_point() + stat_smooth() + ggtitle(title), file=paste("charts/", f, ".svg", sep=""))
 }
